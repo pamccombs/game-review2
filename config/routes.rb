5860fa3b84b59f8e28c7f855da4013e45bf380fb
pamccombs <pa.mccombs1@gmail.com>
2019-07-16 15:19:15 -0400

    Create game model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  get 'games/new'
+  get 'games/show'
   devise_for :users, :controllers => {:registrations => "registrations"}
     devise_scope :user do 
       get 'login', to: 'devise/sessions#new'
